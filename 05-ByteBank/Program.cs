b41982db00e637402ad70a08bceb66bab82654ea
FelipeIgansi <felipeigansi2009@hotmail.com>
2020-09-28 22:58:44 -0300

    Aula06-ReferenciasNulas

--- 05-ByteBank/Program.cs
+++ 05-ByteBank/Program.cs
@@ -10,22 +10,38 @@ namespace _05_ByteBank
     {
         static void Main(string[] args)
         {
-            Cliente Gabriela = new Cliente();
+            //Cliente Gabriela = new Cliente();
 
-            Gabriela.nome = "Gabriela";
-            Gabriela.profissao = "Desenvolvedora C#";
-            Gabriela.cpf = "033.333.333-55";
+            // Gabriela.nome = "Gabriela";
+            // Gabriela.profissao = "Desenvolvedora C#";
+            // Gabriela.cpf = "033.333.333-55";
 
             ContaCorrente conta = new ContaCorrente();
+            
 
-            conta.titular = Gabriela; 
+            // conta.titular = Gabriela;
+            // conta.titular = new Cliente();
+
+            // conta.titular.nome = "Gabriela Costa";
+            // conta.titular.cpf = "033.333.333-55";
+            // conta.titular.profissao = "Desenvolvedora C#";
+
             conta.saldo = 500;
             conta.agencia = 535;
             conta.numero = 2565452;
 
+            if (conta.titular == null)
+            {
+                Console.WriteLine("A referencia para < conta.titular > está null");
+            }
+
+            // Console.WriteLine(Gabriela.nome);
+            Console.WriteLine(conta.titular);
+            // Console.WriteLine(conta.titular.nome);
+            // Console.WriteLine(conta.titular.cpf);
+            // Console.WriteLine(conta.titular.profissao);
 
 
-            Console.WriteLine(Gabriela.nome);
         }
     }
 }
