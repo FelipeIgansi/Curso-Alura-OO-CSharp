4c2e3ed5edf27b5c9fe6b33723338577a186934c
FelipeIgansi <felipeigansi2009@hotmail.com>
2020-09-29 00:17:59 -0300

    Aula03-Propiedades_Propriedades Get e Set

---
+++ 06-ByteBank/Cliente.cs
@@ -0,0 +1,10 @@
+namespace _06_ByteBank
+{
+    public class Cliente
+    {
+        public string nome;
+        public string cpf;
+        public string profissao;
+
+    }
+}
---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.Saldo = 50;
+
+            Cliente cliente = new Cliente();
+            cliente.nome = "Felipe";
+            cliente.cpf = "132.232.252-33";
+            cliente.profissao = "Tester";
+
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.nome);
+
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
