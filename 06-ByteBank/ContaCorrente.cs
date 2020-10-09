b7ca1cc49fef429bf4c18de1c63aa3093fca3c20
FelipeIgansi <lipeigansi@gmail.com>
2020-10-09 13:27:26 -0300

    Aula06-Construtores e Membros estaticos

--- 06-ByteBank/ContaCorrente.cs
+++ 06-ByteBank/ContaCorrente.cs
@@ -1,28 +1,32 @@
-﻿namespace _06_ByteBank
-{
+﻿// using _05_ByteBank;
 
+namespace _06_ByteBank
+{
     public class ContaCorrente
     {
-        public Cliente Titular{get; set; }
-
+        public Cliente Titular { get; set; }
         public int Agencia { get; set; }
-
         public int Numero { get; set; }
-
+        
         private double _saldo = 100;
 
         public double Saldo
         {
-            get { return _saldo; }
+            get
+            {
+                return _saldo;
+            }
             set
             {
                 if (value < 0)
-                { return; }
+                {
+                    return;
+                }
+
                 _saldo = value;
             }
         }
 
-     
         public bool Sacar(double valor)
         {
             if (_saldo < valor)
@@ -52,5 +56,4 @@
             return true;
         }
     }
-
 }
